079bd52fc23dfb15d342ea68d5e3d6393a96edb9
jiang1717 <jiang1717@aliyun.com>
2021-01-07 22:12:33 +0800

    modify classifications and files

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,21 +15,51 @@ module.exports = {
   base: '/vuepressNote/',
   themeConfig: {
     nav: [
-      { text: 'Java基础', link: '/JavaFundamental/' },
-      { text: 'JVM', link: '/JVM/' },
-      { text: 'Java并发', link: '/JavaConcurrency/'},
-      { text: '数据库', link: '/DataBase/'},
-      { text: 'Redis缓存', link: '/Redis/'},
-      { text: '设计模式', link: '/DesignPattern/'},
+      { text: 'JavaSE', link: '/JavaSE/' },
+      { text: 'MySQL', link: '/MySQL/'},
+      { text: 'Redis', link: '/Redis/'},
       { text: '计算机网络', link: '/Network/'},
       { text: '操作系统', link: '/OperatingSystem/' },
       { text: '数据结构与算法', link: '/DataStructure/'}
     ],
     displayAllHeaders: false,
-    sidebar: 'auto', // 侧边栏配置
     sidebarDepth: 2, // 侧边栏显示2级
     collapsable: true,
     docsRepo: 'jiang1717/vuepressNote/',
-    docsDir: '/'
+    docsDir: '/',
+    sidebar:{
+
+      '/JavaSE/':[
+        {
+          title: 'Java基础知识',  //组名
+          children: ['基础01', '基础02', '基础03', '基础04'],   //该分组下要显示的文件的目录
+        },
+        {
+          title: 'Java集合框架',
+          children: ['02_Java集合框架'],
+        },
+        {
+          title: 'Java多线程',
+          children: ['03_Java多线程'],
+        },
+        {
+          title: 'Java虚拟机',
+          children: ['04_Java虚拟机'],
+        },
+        {
+          title: '设计模式',
+          children: ['05_设计模式'],
+        }],
+
+       '/DataStructure/':[
+        {
+          title: '剑指offer',  //组名
+          children: ['剑指001', '剑指002'],   //该分组下要显示的文件的目录
+        },
+        {
+          title: 'leetcode',
+          children: ['leetcode'],
+        }],
+    }
   }
 }
